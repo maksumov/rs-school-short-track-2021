20e3452bc70debb99e6e589a64febd8fc1ec9372
Marat Maksumov <maksumov@users.noreply.github.com>
2021-04-07 18:49:22 +0300

    feat: add solution for 04-mine-sweeper subtask

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,29 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+
+/**
+ *
+ * @param {Number} x - x coordinate of field
+ * @param {Number} y - y coordinate of field
+ * @param {Number} lenX - size of field by x coordinate
+ * @param {Number} lenY - size of field by y coordinate
+ * @returns coords of all neighborhood field cells
+ */
+function neighborsList(x, y, lenX, lenY) {
+  const maybeNeighbors = [
+    [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+    [x - 1, y], [x + 1, y],
+    [x - 1, y + 1], [x, y + 1], [x + 1, y + 1],
+  ];
+
+  return maybeNeighbors.filter(([x2, y2]) => x2 >= 0 && x2 < lenX && y2 >= 0 && y2 < lenY);
+}
+
+function minesweeper(matrix) {
+  return matrix.map((_, y) => _
+    .map(($, x) => neighborsList(x, y, _.length, matrix.length)
+      .reduce((a, [x2, y2]) => a + matrix[y2][x2], 0)));
 }
 
 module.exports = minesweeper;
