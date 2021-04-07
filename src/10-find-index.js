d275edc146fe8dd9d5a27ba965b2df5dab4d9baa
Marat Maksumov <maksumov@users.noreply.github.com>
2021-04-07 21:07:44 +0300

    feat: add solution for 10-find-index subtask

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,23 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let pos = Math.ceil(array.length / 2);
+  let step = Math.ceil(pos / 2);
+
+  while (array[pos] - value) {
+    if (step < 1) step = 1;
+    if (array[pos] > value) {
+      pos -= step;
+    } else {
+      pos += step;
+    }
+    if (pos < 0) pos = 0;
+    if (pos > array.length - 1) pos = array.length - 1;
+    step = Math.ceil(step / 2);
+  }
+
+  return pos;
 }
 
 module.exports = findIndex;
